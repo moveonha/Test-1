56ead8775cebc2202d119d6506e4f73b65011194
lstlove9804 <lstlove9804@ednanet.com>
2025-04-06 23:47:20 +0900

    lst add / server.js에 user 라우터 추가 및 User폴더 및 user.js 추가

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 const express = require('express');
-const cors = require('cors');  // CORS 미들웨어 추가
-const app = express();
-const fs = require('fs');
+const app     = express();
+const port    = 3000;
+const fs      = require('fs');
 
-// CORS 미들웨어 사용
-app.use(cors());  // 모든 도메인에서의 요청을 허용
+const cors    = require('cors');
+app.use(cors());  
 
-const port = 3000;
-
-app.get('/user/:userid', (req, res) => {
-    console.log(req.params);
-    const userId = req.params.userid;
-    res.status(200).send(`userId : ${userId}`);
-});
+const userRouter = require('./User/user');
+app.use('/user', userRouter);
 
 app.listen(port, () => {
     console.log("server Start at 3000 port");
